177b2f4dc8473689e31c9db0a2cc2fb365b7e14b
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2021-11-21 12:49:55 -0500

    Cambio de algoritmo
    El algoritmo con "for" se sustituye por "switch" que es más simple y eficiente.

--- ProcesoUnificado_SistemaPagoSuscripcionGYM/src/Membresia.java
+++ ProcesoUnificado_SistemaPagoSuscripcionGYM/src/Membresia.java
@@ -1,22 +1,31 @@
 import java.util.Arrays;
 import java.util.List;
 
-
 public class Membresia {
-
     private String tipo;
     private double precio;
 
-    public Membresia(String nuevaMembresia) {
-
-        List<String> membresias = Arrays.asList("Anual", "Mensual", "Semanal");
-
-        for(String s : membresias){
-            if (s.equals(nuevaMembresia)) {
-                this.tipo = s;
-                break;
-            }else{
+    public Membresia(String tipo) {
+        establecerTipoMembresia(tipo);
+    }
+    
+    public void establecerTipoMembresia(String tipo){
+        switch (tipo) {
+            case "Anual" -> {
+                this.tipo = "Anual";
+                precio = 200;
+            }
+            case "Mensual" -> {
+                this.tipo = "Mensual";
+                precio = 20;
+            }
+            case "Semanal" -> {
+                this.tipo = "Semanal";
+                precio = 6;
+            }
+            default -> {
                 this.tipo = "No valido";
+                precio = 0;
             }
         }
     }
