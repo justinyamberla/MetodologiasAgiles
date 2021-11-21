682795c25bbd987d257209b9ad204505b5f39c60
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2021-11-21 10:02:01 -0500

    Refactor 1
    Nuevo método establecerTipoMembresia()

--- ProcesoUnificado_SistemaPagoSuscripcionGYM/src/Membresia.java
+++ ProcesoUnificado_SistemaPagoSuscripcionGYM/src/Membresia.java
@@ -1,32 +1,32 @@
-import java.util.*;
-
-
 public class Membresia {
-
     private String tipo;
-    //private String fechInicio;
+    //private String fechaInicio;
     private double precio;
 
-
     public Membresia(String tipo) {
-        if (tipo.equals("Anual")){
-            this.tipo="Anual";
-            precio=200;
-
-        } else if(tipo.equals("Mensual")){
-            this.tipo="Mensual";
-            precio=20;
-
-        } else if (tipo.equals("Semanal")){
-            this.tipo="Semanal";
-            precio=6;
-        }  else {
-            this.tipo="No valido";
-            precio=0;
-        }
-
+        establecerTipoMembresia(tipo);
     }
 
+    public void establecerTipoMembresia(String tipo){
+        switch (tipo) {
+            case "Anual" -> {
+                this.tipo = "Anual";
+                precio = 200;
+            }
+            case "Mensual" -> {
+                this.tipo = "Mensual";
+                precio = 20;
+            }
+            case "Semanal" -> {
+                this.tipo = "Semanal";
+                precio = 6;
+            }
+            default -> {
+                this.tipo = "No valido";
+                precio = 0;
+            }
+        }
+    }
 
     @Override
     public String toString() {
